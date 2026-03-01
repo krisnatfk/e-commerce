4982ae2d47352442f5aa3d1f387f399e1ddd3923
krisnatfk <k1215na77@gmail.com>
2026-03-01 20:11:05 +0700

    ganti repo

--- assets/assets.js
+++ assets/assets.js
@@ -4,6 +4,9 @@ import upload_area from "./upload_area.svg"
 import hero_model_img from "./hero_model_img.png"
 import hero_product_img1 from "./hero_product_img1.png"
 import hero_product_img2 from "./hero_product_img2.png"
+import ordernow from "./benner-ayam.png"
+import ayam1 from "./ayam1.png"
+import ayam2 from "./ayam2.png"
 import product_img1 from "./product_img1.png"
 import product_img2 from "./product_img2.png"
 import product_img3 from "./product_img3.png"
@@ -25,10 +28,11 @@ export const assets = {
     upload_area, hero_model_img,
     hero_product_img1, hero_product_img2, gs_logo,
     product_img1, product_img2, product_img3, product_img4, product_img5, product_img6,
-    product_img7, product_img8, product_img9, product_img10, product_img11, product_img12,
+    product_img7, product_img8, product_img9, product_img10, product_img11, product_img12, ordernow, ayam1, ayam2
 }
 
-export const categories = ["Headphones", "Speakers", "Watch", "Earbuds", "Mouse", "Decoration"];
+// export const categories = ["Headphones", "Speakers", "Watch", "Earbuds", "Mouse", "Decoration"];
+export const categories = ["Dada Aym", "Paha Aym", "Ceker Aym", "Sayap", "Utuh", "Kepala"];
 
 export const dummyRatingsData = [
     { id: "rat_1", rating: 4.2, review: "I was a bit skeptical at first, but this product turned out to be even better than I imagined. The quality feels premium, it's easy to use, and it delivers exactly what was promised. I've already recommended it to friends and will definitely purchase again in the future.", user: { name: 'Kristin Watson', image: profile_pic1 }, productId: "prod_1", createdAt: 'Sat Jul 19 2025 14:51:25 GMT+0530 (India Standard Time)', updatedAt: 'Sat Jul 19 2025 14:51:25 GMT+0530 (India Standard Time)', product: { name: 'Bluetooth Speakers', category:'Electronics', id:'prod_1'} },
